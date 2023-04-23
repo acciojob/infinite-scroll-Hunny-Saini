3855d68f30ccca0274c1443cf0e726948dea52dc
Hunny Saini <124673671+Hunny-Saini@users.noreply.github.com>
2023-04-23 13:51:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 //your code here!
+list.addEventListener('scroll', event => {
+    const {scrollHeight, scrollTop, clientHeight} = event.target;
 
+    if (Math.abs(scrollHeight - clientHeight - scrollTop) < 1) {
+        appendlist();
+        appendlist();
+    }
+});
